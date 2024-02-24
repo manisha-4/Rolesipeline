4e08847e5f3fd4bb07a8434a644fa5ac821e3dc0
Manisha Kumari Sah <60925015+manisha-4@users.noreply.github.com>
2024-02-24 19:49:35 +0530

    Update main.tf

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,1 +1,9 @@
+resource "aws_iam_openid_connect_provider" "githubOidc" {
+ url = "https://token.actions.githubusercontent.com"
 
+ client_id_list = [
+   "sts.amazonaws.com"
+ ]
+
+ thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+}
